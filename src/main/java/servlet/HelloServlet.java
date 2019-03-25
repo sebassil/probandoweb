52818051ac3a677c5fe47a40e154c986ee07964b
sebassil <48248691+sebassil@users.noreply.github.com>
2019-03-25 18:56:37 -0500

    sebas

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -15,14 +15,7 @@ import javax.servlet.http.HttpServletResponse;
     )
 public class HelloServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        ServletOutputStream out = resp.getOutputStream();
-        out.println("aqui");
-        out.write("hello heroku".getBytes());
-        out.flush();
-        out.close();
-    }
+    out.println("aqui");
+    
     
 }
