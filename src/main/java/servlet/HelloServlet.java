4fba9af1370ace205bea4b4a8dd0bca965cba7e8
sebassil <48248691+sebassil@users.noreply.github.com>
2019-03-25 21:57:32 -0500

    sebas

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -8,21 +8,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 
 @WebServlet(
         name = "MyServlet", 
-        urlPatterns = {"/adicionaremp"}
+        urlPatterns = {"/hello"}
     )
-
-public class Adicionaremp extends HttpServlet {
-
+public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
 
-        out.println("sebas");
+        out.write("hello heroku".getBytes());
+        out.flush();
+        out.close();
     }
     
 }
