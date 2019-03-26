905455a36f505b78c91c3492e0b684a18926a1f4
sebassil <48248691+sebassil@users.noreply.github.com>
2019-03-26 14:12:57 -0500

    sebas

---
+++ src/main/java/servlet/Relacion.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Relacion(){
+    private String id;
+    private String codigo;
+   
+    
+    public Relacion(){
+        this.id = "";
+        this.codigo = "";
+    }
+
+    public Relacion(String id,String codigo){
+        this.id = id;
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return this.id+","+this.codigo;
+    }
+}
--- src/main/java/servlet/Simple.java
+++ src/main/java/servlet/Simple.java
@@ -14,6 +14,7 @@ public class Simple{
     private static Simple instancia=null;
     ArrayList<Empleado> empleados = new ArrayList<Empleado>();
     ArrayList<Proyecto> proyectos = new ArrayList<Proyecto>();
+    ArrayList<Relacion> relaciones = new ArrayList<Relacion>();
 
     public static Simple laconstructora(){
         if(instancia==null)
@@ -61,5 +62,36 @@ public class Simple{
             men="no existe";
         }
         return men;
+    }
+
+    public String adicionarrel(String a,String b){
+        String m="";
+        boolean emp = false;
+        boolean pro = false;
+
+        for(int i=0;i<empleados.size();i++){
+            if(empleados.get(i).getCodigo().equals(a)){
+                emp=true;
+            }              
+        }
+
+        for(int j=0;j<proyectos.size();j++){
+            if(proyectos.get(i).getCodigo().equals(a)){
+                pro=true;
+            }              
+        }
+
+        if(emp){
+            if(pro){
+                Relacion nuevo = new Relacion(a,b);
+                relaciones.add(nuevo);
+                m=nuevo.toString();
+            }else{
+                m="No existe un proyecto con este codigo";
+            }
+        }else{
+            m="No existe un empleado con este codigo";
+        }
+        return m;
     }
 }
