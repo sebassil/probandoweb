ba00050888943cec6ff790cf82d2e1188c7e0a2f
sebassil <48248691+sebassil@users.noreply.github.com>
2019-04-08 18:50:35 -0500

    sebas

--- src/main/java/servlet/Simple.java
+++ src/main/java/servlet/Simple.java
@@ -69,6 +69,10 @@ public class Simple{
             }    
         }
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
+        Date date1 = sdf.parse(c); 
+        Date date2 = sdf.parse(d);
+        Date date3 = sdf.parse("1995-02-23");
         //Fechas
         if(!c.equals("")){
             for(int j=0;j<productos.size();j++){
