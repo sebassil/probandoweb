e3fe6a5ef27aa8f20ef23c236a5b794e66450039
sebassil <48248691+sebassil@users.noreply.github.com>
2019-04-08 18:57:17 -0500

    sebas

--- src/main/java/servlet/Simple.java
+++ src/main/java/servlet/Simple.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 
 public class Simple{
     private static Simple instancia=null;
