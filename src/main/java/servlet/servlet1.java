2661f42a88851ee9e1a23eb50b5e61ff98a4ccd5
sebassil <48248691+sebassil@users.noreply.github.com>
2019-04-09 16:51:46 -0500

    sebas

--- src/main/java/servlet/servlet1.java
+++ src/main/java/servlet/servlet1.java
@@ -26,6 +26,7 @@ public class servlet1 extends HttpServlet {
         b=req.getParameter("3a");
         c=req.getParameter("4a");
         sebas.adicionarusu(a,b,c);
+        out.println("Guardado");
     }
 }
 
